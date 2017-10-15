3d9632b04ac2ad7024ed4ce228ccf2e7222d6590
Mathilde Barbier <mathilde206@gmail.com>
2017-10-15 11:36:06 +0200

    Add the registration form

---
+++ js/app.js
@@ -0,0 +1,15 @@
+angular.module('TodoApp', ['ngRoute', 'RouteControllers']);
+ 
+angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);  // Enable href routing without hashes
+ 
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    })
+    .when('/accounts/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+});
+
