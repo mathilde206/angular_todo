8de856b1227fc21f3e7d3ba9e1e559ef2a5b9cd4
Mathilde Barbier <mathilde206@gmail.com>
2017-10-17 17:09:01 +0200

    Add todo list (add, edit, delete a todo)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage']);
+angular.module('TodoApp', ['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', "TodoService"]);
 
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
@@ -7,6 +7,17 @@ angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
         templateUrl: 'templates/home.html',
         controller: 'HomeController'
     })
+
+    .when('/todo', {
+        templateUrl: 'templates/todo.html',
+        controller: 'TodoController'
+    })
+
+    .when('/todo/edit/:id', {
+    templateUrl:'templates/edit-todo.html',
+    controller: 'EditTodoController'
+    })
+
     .when('/accounts/register', {
         templateUrl: 'templates/register.html',
         controller: 'RegisterController'
